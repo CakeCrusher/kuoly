603626c8fdb273fb2d7f92900b1fa2ccb8f18031
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-03-20 11:49:42 -0400

    feedback styling

--- client/src/components/Feedback/Feedback.tsx
+++ client/src/components/Feedback/Feedback.tsx
@@ -3,7 +3,7 @@ import { Modal } from "..";
 import useUtilsHooks from "../../graphql/hooks/utils";
 
 import "./feedback.less";
-import { FiSend } from "react-icons/fi";
+import { FiMail, FiSend } from "react-icons/fi";
 
 const Feedback: React.FC = () => {
   const [show, setShow] = useState(false);
@@ -36,47 +36,81 @@ const Feedback: React.FC = () => {
     <div>
       <Modal show={show} close={handleClose}>
         {submitted ? (
-          <Modal.Header>
-            <h3>Feedback submitted. Thanks for your input!</h3>
-          </Modal.Header>
+          <>
+            <Modal.Header close={handleClose}>
+              <div className="feedback-header">
+                Feedback submitted. Thanks for your input!
+              </div>
+            </Modal.Header>
+            <Modal.Footer>
+              <div className="f-row f-center">
+                <button
+                  className="btn  btn-secondary-outline"
+                  onClick={handleClose}
+                >
+                  Close
+                </button>
+              </div>
+            </Modal.Footer>
+          </>
         ) : (
           <>
-            <Modal.Header>
-              <h3>Feedback</h3>
+            <Modal.Header close={handleClose}>
+              <div className="feedback-header">
+                Got feedback on Kuoly? We'd love to hear it.
+              </div>
             </Modal.Header>
             <Modal.Body>
-              <label htmlFor="feedback">Any thoughts to share?</label>
-              <textarea
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                id="feedback"
-                rows={3}
-                placeholder="Send feedback..."
-              />
-              <input
-                id="feeback-checkbox"
-                type="checkbox"
-                checked={followUp}
-                onChange={(e) => setFollowUp(e.target.checked)}
-              />
-              <label htmlFor="feeback-checkbox">
-                May we follow up with you?
-              </label>
-              {followUp && (
-                <div>
-                  <label htmlFor="email">Email</label>
+              <div className="feedback-body">
+                <label htmlFor="feedback">Any thoughts to share?</label>
+                <textarea
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  id="feedback"
+                  rows={3}
+                  placeholder="Send feedback..."
+                  className="textarea"
+                />
+                <div className="input-container-row">
                   <input
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    id="email"
-                    placeholder="Enter email"
+                    id="feeback-checkbox"
+                    type="checkbox"
+                    checked={followUp}
+                    onChange={(e) => setFollowUp(e.target.checked)}
                   />
+                  <label htmlFor="checkbox feeback-checkbox">
+                    May we follow up with you?
+                  </label>
                 </div>
-              )}
+                {followUp && (
+                  <div className="input-icon-container">
+                    <label htmlFor="email">
+                      <FiMail className="icon" />
+                    </label>
+                    <input
+                      className="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      type="email"
+                      id="email"
+                      placeholder="Enter email"
+                    />
+                  </div>
+                )}
+              </div>
             </Modal.Body>
             <Modal.Footer>
-              <button onClick={onSubmit}>Submit</button>
+              <div className="g-row footer">
+                <button
+                  className="btn  btn-secondary-outline"
+                  onClick={handleClose}
+                >
+                  Cancel
+                </button>
+                <button className="btn btn-secondary" onClick={onSubmit}>
+                  Submit
+                </button>
+              </div>
             </Modal.Footer>
           </>
         )}
