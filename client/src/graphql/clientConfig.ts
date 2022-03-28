5014ff6935dd5d298b70c5a7c72ea4661289bd7c
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-03-27 20:27:57 -0400

    .

--- client/src/graphql/clientConfig.ts
+++ client/src/graphql/clientConfig.ts
@@ -7,7 +7,6 @@ import { createUploadLink } from "apollo-upload-client";
 const authorization = localStorage.getItem("authorization");
 if (!authorization) {
   const newUuid = uuidv4();
-  // const newUuid = "6a3a2967-0258-4caf-8fef-f844c060b2f2";
   console.log("newUuid", newUuid);
   localStorage.setItem("authorization", newUuid);
 }
