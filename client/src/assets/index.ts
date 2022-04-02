9302aad29b78453e0dc4b4785fbf63866b0ddc18
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-04-02 16:26:28 -0400

    cleanup

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,26 +1,9 @@
 //@ts-nocheck
-export { default as ZoomIn } from "./svgs/zoom-in.svg";
-export { default as ZoomOut } from "./svgs/zoom-in.svg";
 export { default as Camera } from "./svgs/camera.svg";
 export { default as Palette } from "./svgs/palette.svg";
 export { default as X } from "./svgs/x.svg";
-export { default as Plus } from "./svgs/plus.svg";
 export { default as ChevronDown } from "./svgs/chevron-down.svg";
 export { default as ChevronUp } from "./svgs/chevron-up.svg";
-export { default as ChevronLeft } from "./svgs/chevron-left.svg";
-export { default as ChevronRight } from "./svgs/chevron-right.svg";
-export { default as ChevronsDown } from "./svgs/chevrons-down.svg";
-export { default as ChevronsUp } from "./svgs/chevrons-up.svg";
-export { default as ChevronsLeft } from "./svgs/chevrons-left.svg";
-export { default as ChevronsRight } from "./svgs/chevrons-right.svg";
-export { default as DragHorizontal } from "./svgs/drag-horizontal.svg";
-export { default as Edit } from "./svgs/edit.svg";
-export { default as Edit2 } from "./svgs/edit-2.svg";
-export { default as Edit3 } from "./svgs/edit-3.svg";
-export { default as Eye } from "./svgs/eye.svg";
-export { default as EyeOff } from "./svgs/eye-off.svg";
-export { default as Trash } from "./svgs/trash.svg";
 export { default as Trash2 } from "./svgs/trash-2.svg";
-export { default as Share } from "./svgs/share.svg";
 export { default as Share2 } from "./svgs/share-2.svg";
 export { default as BannerLogo } from "./icon/home_banner_logo.svg";
